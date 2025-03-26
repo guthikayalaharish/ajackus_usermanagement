9390b3e35d142c024b07470cb4a0b8654127288b
G R HARISH <guthikayalaharish@gmail.com>
2025-03-26 11:20:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/u");
+    const response = await fetch("");
     const data = await response.json();
 
     const usersWithFirstLastNames = data.map((user, index) => {
