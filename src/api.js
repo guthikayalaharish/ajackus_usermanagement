61c6f4a5ddad65315ff1200dd376fc5dbbce09db
G R HARISH <guthikayalaharish@gmail.com>
2025-03-26 11:13:04 +0530

    api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "https://jsonplaceholder.typicode.com/us";
-
+//const API_URL = "https://jsonplaceholder.typicode.com/users";
+const API_URL = ""
 export const fetchUsers = async () => {
   try {
     const response = await axios.get(API_URL);
