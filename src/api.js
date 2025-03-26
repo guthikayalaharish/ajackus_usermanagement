a5c4f510f78e31eb405aef5fb240ee4fe807d36c
G R HARISH <guthikayalaharish@gmail.com>
2025-03-26 11:19:50 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,15 @@
 
+import axios from "axios";
+
+//const API_URL = "https://jsonplaceholder.typicode.com/users";
+const API_URL = ""
+export const fetchUsers = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+// You can add more API functions here if necessary for Add, Edit, Delete.
